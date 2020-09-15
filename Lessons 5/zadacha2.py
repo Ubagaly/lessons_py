84701c2e3141bcaa4749a65c6d0e17abe03c85aa
Ubagaly <ubagaly@gmail.com>
2020-09-15 15:03:36 +0300

    Delete lessons 5

--- Lessons 5/zadacha2.py
+++
@@ -1,8 +0,0 @@
-with open("zadacha_2.txt", "r") as test_obj:
-    sum_str = 0
-    for line in test_obj:
-        sum_str = sum_str + 1
-        sum_list = line.split(" ")
-        sum_sl = len(sum_list)
-        print(line)
-        print(f"В {sum_str} строке {sum_sl} слов(а)")
